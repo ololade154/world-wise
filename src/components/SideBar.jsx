a721de16b96481a6cd8d356844f72fd60b0a80b1
Ololade154 <shogbaikeoluwakemi2020@gmail.com>
2025-09-06 14:46:15 +0100

    update

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,15 @@
+import AppNav from './AppNav';
+import Footer from './Footer';
+import Logo from './Logo';
+import styles from './SideBar.module.css';
+function SideBar() {
+  return (
+    <div className={styles.sidebar}>
+      <Logo />
+      <AppNav />
+      <p>List Of Cities</p>
+      <Footer />
+    </div>
+  );
+}
+export default SideBar;
